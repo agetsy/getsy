6f8ba48c903a176f98753210c3ee3fcaa51153a5
omrllm <omarallam@protonmail.com>
2019-01-19 16:29:51 +0100

    Add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("Getsy"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.change_history),
+                title: Text('Change history'),
+                onTap: () {
+                  // change app state...
+                  Navigator.pop(context); // close the drawer
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
